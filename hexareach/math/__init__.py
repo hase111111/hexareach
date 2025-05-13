1da6dd687ab076b848f10f31cb3073a1a22fd5f0
hase111111 <hasehasehase61@gmail.com>
2025-05-13 15:38:51 +0900

    Update: mathに__init__.pyを追加

---
+++ hexareach/math/__init__.py
@@ -0,0 +1,17 @@
+"""
+__init__.py
+"""
+
+# Copyright (c) 2023-2025 Taisei Hasegawa
+# Released under the MIT license
+# https://opensource.org/licenses/mit-license.php
+
+
+from .triangle_checker import TriangleChecker
+
+# パッケージのバージョンはsetup.pyに記載
+# The package version is specified in setup.py
+
+__all__ = [
+    "TriangleChecker",
+]
