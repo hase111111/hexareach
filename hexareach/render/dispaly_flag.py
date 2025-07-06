0ee6a5d3eb06fd645f3579d308bd3b46c8847140
hase111111 <hasehasehase61@gmail.com>
2025-07-07 00:13:04 +0900

    Feat: DisplayFlagを追加

---
+++ hexareach/render/dispaly_flag.py
@@ -0,0 +1,16 @@
+"""
+display_flag.py
+"""
+
+# Copyright (c) 2023-2025 Taisei Hasegawa
+# Released under the MIT license
+# https://opensource.org/licenses/mit-license.php
+
+class DisplayFlag:
+    """
+    グラフの表示に関するフラグを格納するクラス.
+    """
+
+    approximated_graph_filled: bool = True
+    leg_circle_displayed: bool = True
+    leg_wedge_displayed: bool = True
