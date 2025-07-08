b71c70b129c23425eaa03b158ffd67b98eff3310
hase111111 <hasehasehase61@gmail.com>
2025-07-08 17:34:50 +0900

    Feat LegPowerCalcを追加

---
+++ hexareach/calc/leg_power_calculator.py
@@ -0,0 +1,12 @@
+"""
+leg_power_calculator.py
+"""
+
+# Copyright (c) 2023-2025 Taisei Hasegawa
+# Released under the MIT license
+# https://opensource.org/licenses/mit-license.php
+
+class LegPowerCalculator:
+    """
+    脚先が出すことができる力を計算するクラス.
+    """
