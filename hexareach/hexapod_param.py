fbb5fda9ad6887817a6906a597163f71c39f7abf
hase111111 <hasehasehase61@gmail.com>
2025-07-08 14:01:56 +0900

    Feat: PhantomXParamを追加

--- hexareach/hexapod_param.py
+++ hexareach/hexapod_param.py
@@ -6,7 +6,6 @@ hexapod_param.py
 # Released under the MIT license
 # https://opensource.org/licenses/mit-license.php
 
-import math
 from typing import Protocol
 
 
@@ -26,20 +25,3 @@ class HexapodParamProtocol(Protocol):
     theta3_min: float
     torque_max: float
     approx_min_radius: float
-
-
-class HexapodParam(HexapodParamProtocol):
-    """
-    Class for storing PhantomX parameters.
-    """
-    coxa_length: float = 52.0  # [mm]
-    femur_length: float = 66.0  # [mm]
-    tibia_length: float = 130.0  # [mm]
-    theta1_max: float = math.radians(81.0)  # [rad]
-    theta1_min: float = math.radians(-81.0)  # [rad]
-    theta2_max: float = math.radians(99.0)  # [rad]
-    theta2_min: float = math.radians(-105)  # [rad]
-    theta3_max: float = math.radians(25.5)  # [rad]
-    theta3_min: float = math.radians(-145.0)  # [rad]
-    torque_max: float = 1800.0  # [N*mm] ストールトルク(停動トルク)
-    approx_min_radius: float = 140.0  # [mm]
---
+++ hexareach/phatomx_mk2_param.py
@@ -0,0 +1,28 @@
+"""
+phantomx_mk2_param.py
+"""
+
+# Copyright (c) 2023-2025 Taisei Hasegawa
+# Released under the MIT license
+# https://opensource.org/licenses/mit-license.php
+
+import math
+
+from .hexapod_param import HexapodParamProtocol
+
+
+class PhantomxMk2Param(HexapodParamProtocol):
+    """
+    Class for storing PhantomX parameters.
+    """
+    coxa_length: float = 52.0  # [mm]
+    femur_length: float = 66.0  # [mm]
+    tibia_length: float = 130.0  # [mm]
+    theta1_max: float = math.radians(81.0)  # [rad]
+    theta1_min: float = math.radians(-81.0)  # [rad]
+    theta2_max: float = math.radians(99.0)  # [rad]
+    theta2_min: float = math.radians(-105)  # [rad]
+    theta3_max: float = math.radians(25.5)  # [rad]
+    theta3_min: float = math.radians(-145.0)  # [rad]
+    torque_max: float = 1800.0  # [N*mm] ストールトルク(停動トルク)
+    approx_min_radius: float = 140.0  # [mm]
