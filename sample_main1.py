ae0fe75a40c50bb43a826f453569603f0f611732
hase111111 <hasehasehase61@gmail.com>
2025-07-09 16:51:20 +0900

    Docs: コメントを追加

--- sample_main1.py
+++ sample_main1.py
@@ -18,6 +18,8 @@ if __name__ == "__main__":
     # display 関数を呼ぶことで描画されます．
     # マウスカーソルを動かすことで，脚先の位置を変化させることができます.
     # 左クリックすることで result/sample_main1.png に画像を保存できます.
+    # 右クリックすることで, 左クリック時に生成される軌跡を削除することができます.
+    # ホイールクリックによって，逆運動学解を反転させることができます.
     graph.display(
         hxr.PhantomxMk2Param(),
         image_file_name="result/sample_main1.png")
