2a4d05ad7c4befa9ee1210ec9483aee7e89c4c0c
hase111111 <hasehasehase61@gmail.com>
2025-07-09 17:09:43 +0900

    引数でFigを受けとることができるように変更 #11

--- hexareach/graph_dispalyer.py
+++ hexareach/graph_dispalyer.py
@@ -6,7 +6,7 @@ graph_dispalyer.py
 # Released under the MIT license
 # https://opensource.org/licenses/mit-license.php
 
-from typing import Tuple
+from typing import Tuple, Optional
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -41,81 +41,36 @@ class GraphDisplayer:
         leg_power_step: float =2.0,
         image_file_name: str="result/img_main.png",
         ground_z: float =-25.0,
-        do_not_show: bool =False
-    ) -> Tuple[Figure, Axes, Axes]:
+        do_not_show: bool =False,
+        figure : Optional[Figure] = None,
+        axes: Optional[Axes] = None,
+        axes_table: Optional[Axes] = None,
+    ):
         """
         x_min < x < x_max , z_min < z < z_max の範囲でグラフを描画する．\n
         変数の値を変更することで処理の内容を変更できる．
-
-        Parameters
-        ----------
-        display_table: bool
-            表を表示するか．
-        display_leg_power: bool
-            脚が出せる力のグラフを表示するか．
-        display_approximated_graph: bool
-            脚の可動範囲の近似値を表示するか．
-        display_circle: bool
-            脚の可動域を円で表示するか．
-        display_wedge: bool
-            可動範囲を扇形で表示するか．
-        display_ground_line: bool
-            地面の線を表示するか．
-        display_mouse_grid: bool
-            マウスがグラフのどこをポイントしているかを示す線を表示するか．
-        x_min: float
-            x軸の最小値．
-        x_max: float
-            x軸の最大値．
-        z_min: float
-            z軸の最小値．
-        z_max: float
-            z軸の最大値．
-        leg_power_step: float
-            何mmごとに力の分布を計算するか．
-        approx_fill: bool
-            脚の可動範囲の近似値を塗りつぶすかどうか．
-        color_approx: str
-            脚の可動範囲の近似値の色．
-        alpha_approx: float
-            脚の可動範囲の近似値の透明度．
-        color_rom: str
-            脚の可動範囲の色．
-        alpha_upper_rom: float
-            脚の可動範囲の上側の透明度．
-        alpha_lower_rom: float
-            脚の可動範囲の下側の透明度．
-        color_mouse_grid: str
-            マウスがグラフのどこをポイントしているかを示す線の色．
-        alpha_mouse_grid: float
-            マウスがグラフのどこをポイントしているかを示す線の透明度．
-        image_file_name: str
-            脚を画像で表示する場合の画像ファイル名.
-        ground_z: float
-            地面の高さ．
-        do_not_show: bool
-            show()を実行しない場合にTrueにする．
-
-        Returns
-        -------
-        fig : matplotlib.figure.Figure
-            グラフのfigureオブジェクト．
-        ax : matplotlib.axes.Axes
-            グラフのaxesオブジェクト．
-        ax_table : matplotlib.axes.Axes
-            表のaxesオブジェクト．
         """
 
         if display_flag.display_table:
-            fig = plt.figure()  # type: ignore
-            ax = fig.add_subplot(1, 2, 1)  # type: ignore
-            ax_table = fig.add_subplot(1, 2, 2)  # type: ignore
+            if figure is None or axes is None or axes_table is None:
+                fig = plt.figure()  # type: ignore
+                ax = fig.add_subplot(1, 2, 1)  # type: ignore
+                ax_table = fig.add_subplot(1, 2, 2)  # type: ignore
+            else:
+                fig = figure
+                ax = axes
+                ax_table = axes_table
         else:
-            fig = plt.figure()  # type: ignore
-            ax = fig.add_subplot(1, 1, 1)  # type: ignore
-            # 今回は使用しないので適当な座標に配置.
-            ax_table = fig.add_subplot(3, 3, 2)  # type: ignore
-            ax_table.set_visible(False)  # 表示しない.
+            if figure is None or axes is None:
+                fig = plt.figure()  # type: ignore
+                ax = fig.add_subplot(1, 1, 1)  # type: ignore
+                # 今回は使用しないので適当な座標に配置.
+                ax_table = fig.add_subplot(3, 3, 2)  # type: ignore
+                ax_table.set_visible(False)  # 表示しない.
+            else:
+                fig = figure
+                ax = axes
+                ax_table = axes
 
         # 以下グラフの作成，描画.
         hexapod_calc = HexapodLegRangeCalculator(hexapod_pram)
@@ -178,5 +133,3 @@ class GraphDisplayer:
 
         if not do_not_show:
             plt.show()  # type: ignore
-
-        return fig, ax, ax_table
